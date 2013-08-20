998af90478384751677dc2befb86f103936b9f75
danialf <danial.farid@gmail.com>
2013-08-20 17:00:52 -0400

    added minified and verbose version

--- angular-file-upload.js
+++ angular-file-upload.js
@@ -1,6 +1,114 @@
-/**!
- * AngularJS file upload directive and http post
- * @author  Danial  <danial.farid@gmail.com>
- * version 0.1.1
- */
-var angularFileUpload=angular.module("angularFileUpload",[]);(function(){if(typeof jQuery==="undefined"){var t=document.createElement("script");t.setAttribute("src","//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js");document.getElementsByTagName("head")[0].appendChild(t)}if(typeof FormData==="undefined"){var t=document.createElement("script");var n=document.getElementsByTagName("script");for(var r=0;r<n.length;r++){var i=n[r].src.indexOf("angular-file-upload.js");if(i>-1){base=n[r].src.substring(0,i)}}if(typeof FileAPI==="undefined"||FileAPI.staticPath==null){FileAPI={staticPath:base}}t.setAttribute("src",base+"FileAPI.min.js");document.getElementsByTagName("head")[0].appendChild(t)}})();angularFileUpload.directive("ngFileSelect",["$parse","$http",function(e,t){if(t.uploadFile===undefined){t.uploadFile=function(e){if(typeof FormData==="undefined"){var n={url:e.url,data:e.data,complete:function(t,r){var i=/^\s*(\[|\{[^\{])/,s=/[\}\]]\s*$/,o=/^\)\]\}',?\n/;var u=r.responseText;if(typeof u=="string"){u=u.replace(o,"");if(i.test(u)&&s.test(u))u=typeof u=="string"?JSON.parse(u):u}if(n.promiseThen!=null)n.promiseThen(u,r.status,null,e);if(n.promiseError!=null)n.promiseError(u,r.status,null,e);if(n.promiseSuccess!=null)n.promiseSuccess(u,r.status,null,e)}};if(e.file!=null){n.files={file:e.file}}var r=FileAPI.upload(n);return{then:function(e){n.promiseThen=e},success:function(e){n.promiseSuccess=e},error:function(e){n.promiseError=e}}}else{return t({method:"POST",url:e.url,headers:{"Content-Type":false},transformRequest:function(t){var n=new FormData;n.append("file",e.file);for(key in e.data){n.append(key,e.data[key])}return n}})}}}return function(t,n,r){if(typeof FormData==="undefined"){n.wrap('<div class="js-fileapi-wrapper" style="position:relative; overflow:hidden">')}var i=e(r["ngFileSelect"]);n.bind("change",function(e){var n=[];if(typeof FormData!=="undefined"){var r=e.target.files;for(var s=0;s<r.length;s++){n.push(r.item(s))}}else{n=FileAPI.getFiles(e)}t.$apply(function(){i(t,{$files:n,$event:e})})})}}])
+var angularFileUpload = angular.module('angularFileUpload', []);
+
+(function loadFileAPI() {
+    if (typeof jQuery === 'undefined') {
+        var script = document.createElement('script');
+        script.setAttribute('src', '//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js');
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+
+    if (typeof FormData === 'undefined') {
+        var script = document.createElement('script');
+        var allScripts = document.getElementsByTagName('script');
+        for(var i = 0; i < allScripts.length; i++) {
+            var index = allScripts[i].src.indexOf('angular-file-upload.js')
+            if (index > -1) {
+                base = allScripts[i].src.substring(0, index);
+            }
+        }
+        
+        if (typeof FileAPI === "undefined" || FileAPI.staticPath == null) {
+        	FileAPI = {
+        		staticPath: base
+        	}
+        }
+
+        script.setAttribute('src', base + 'FileAPI.min.js');
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+})()
+
+angularFileUpload.directive('ngFileSelect', [ '$parse', '$http', function($parse, $http) {
+    if ($http.uploadFile === undefined) {
+        $http.uploadFile = function(config) {
+            if (typeof FormData === 'undefined') {
+                var params = {
+                    url: config.url,
+                    data: config.data,
+                    complete: function (err, xhr) {
+                        var JSON_START = /^\s*(\[|\{[^\{])/,
+                              JSON_END = /[\}\]]\s*$/,
+                              PROTECTION_PREFIX = /^\)\]\}',?\n/;
+                        var data = xhr.responseText;
+                        if (typeof data == 'string') {
+                                // strip json vulnerability protection prefix
+                                data = data.replace(PROTECTION_PREFIX, '');
+                                if (JSON_START.test(data) && JSON_END.test(data))
+                                  data = (typeof data == 'string')
+                                               ? JSON.parse(data)
+                                               : data;
+                        }
+                        if (params.promiseThen != null) params.promiseThen(data, xhr.status, null, config);
+                        if (params.promiseError != null) params.promiseError(data, xhr.status, null, config);
+                        if (params.promiseSuccess != null) params.promiseSuccess(data, xhr.status, null, config);
+                    }
+                }
+                if (config.file != null) {
+                    params.files = {
+                        file: config.file
+                    }
+                }
+                var xhr = FileAPI.upload(params);
+                return {
+                    then: function(func) {
+                        params.promiseThen = func;
+                    },
+                    success: function(func) {
+                        params.promiseSuccess = func;
+                    },
+                    error: function(func) {
+                        params.promiseError = func;
+                    }
+                };
+            } else {
+                return $http({
+                    method: 'POST',
+                    url: config.url,
+                    headers: { 'Content-Type': false },
+                    transformRequest: function (data) {
+                        var formData = new FormData();
+                          formData.append('file', config.file);
+                        for (key in config.data) {
+                        	formData.append(key, config.data[key]);
+                        }
+                        return formData;
+                    }
+                });
+            }
+        };
+    }
+
+    return function(scope, elem, attr) {
+        if (typeof FormData === 'undefined') {
+            elem.wrap('<div class="js-fileapi-wrapper" style="position:relative; overflow:hidden">');
+        }
+        var fn = $parse(attr['ngFileSelect']);
+		elem.bind('change', function(evt) {
+		    var files = [];
+            if (typeof FormData !== 'undefined') {
+                var fileList = evt.target.files;
+                for (var i = 0; i < fileList.length; i++) {
+                    files.push(fileList.item(i));
+                }
+            } else {
+                files = FileAPI.getFiles(evt);
+            }
+            scope.$apply(function() {
+                fn(scope, {
+                    $files: files,
+                    $event : evt
+                });
+            });
+		});
+    };
+} ]);
