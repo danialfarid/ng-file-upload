57904c6eb1820673db3eb9a51d140352899b34b0
Danial Farid <dan@Danial.local>
2014-09-24 11:28:30 -0400

    tweaking flash error response without body

--- demo/war/js/angular-file-upload-shim.js
+++ demo/war/js/angular-file-upload-shim.js
@@ -138,7 +138,7 @@ if (window.XMLHttpRequest) {
 										xhr.getAllResponseHeaders = function(){};
 										_this.complete(null, {status: 204, statusText: 'No Content'});
 									}
-								}, 500);
+								}, 3000);
 							}
 						},
 						headers: xhr.__requestHeaders
