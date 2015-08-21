0f51b50342878d86f9816ae7e038e9589ccf0fbb
Danial Farid <dan@Danial.local>
2015-08-21 17:53:12 -0400

    fixed  #956

--- src/shim-upload.js
+++ src/shim-upload.js
@@ -97,7 +97,7 @@
             jsonp: false, //removes the callback form param
             cache: true, //removes the ?fileapiXXX in the url
             complete: function (err, fileApiXHR) {
-              if (err.indexOf('#2174') !== -1) {
+              if (err && angular.isString(err) && err.indexOf('#2174') !== -1) {
                 // this error seems to be fine the file is being uploaded properly.
                 err = null;
               }
