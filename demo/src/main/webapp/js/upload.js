c270e625488595d0154fde6ee7036551751107d0
Danial Farid <dan@Danial.local>
2015-09-19 16:24:54 -0400

    updated demo

--- demo/src/main/webapp/js/upload.js
+++ demo/src/main/webapp/js/upload.js
@@ -2,7 +2,7 @@
 
 
 var app = angular.module('fileUpload', ['ngFileUpload']);
-var version = '7.0.16';
+var version = '7.3.2';
 
 app.controller('MyCtrl', ['$scope', '$http', '$timeout', '$compile', 'Upload', function ($scope, $http, $timeout, $compile, Upload) {
   $scope.usingFlash = FileAPI && FileAPI.upload != null;
